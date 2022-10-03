0973134a63a40483c4981a8a51b9f31b0a91c86c
Buddhika Chathuranga <vmbckscc456@gmail.com>
2022-10-03 23:19:37 +0530

    Update get all teams API

--- team/urls.py
+++ team/urls.py
@@ -5,8 +5,5 @@ from .views import TeamView, TeamsView
 team_urlpatterns = [
     path('<int:pk>', TeamView.as_view()),   # get(get team by pk)
     path('', TeamView.as_view()),           # post(create team)
-]
-
-teams_urlpatterns = [
-    path('', TeamsView.as_view()),           # get(get all teams)
+    path('all/', TeamsView.as_view())       # get(get all teams)
 ]
