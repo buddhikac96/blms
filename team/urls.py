242a661709e0175548a8ec9da0d13ee4f9e62a43
Buddhika Chathuranga <vmbckscc456@gmail.com>
2022-10-04 07:04:45 +0530

    Add 90th percentile API

--- team/urls.py
+++ team/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TeamView, TeamListView
+from .views import TeamView, TeamListView, TeamStats90thPercentile
 
 
 team_urlpatterns = [
     path('<int:pk>', TeamView.as_view()),   # get(get team by pk)
     path('', TeamView.as_view()),           # post(create team)
-    path('all/', TeamListView.as_view())       # get(get all teams)
+    path('all/', TeamListView.as_view()),       # get(get all teams)
+    path('stats/90thpercentile/<int:pk>', TeamStats90thPercentile.as_view())
 ]
--- team/views.py
+++ team/views.py
@@ -1,9 +1,14 @@
+import math
+
 from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Team
 from .serializer import TeamSerializer
+from user.models import Player
+from league.models import MatchPlayerConnection
+from user.serializers import PlayerSerializer
 
 
 class TeamView(APIView):
@@ -60,3 +65,34 @@ class TeamListView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response('No teams exists', status=status.HTTP_204_NO_CONTENT)
 
+
+class TeamStats90thPercentile(APIView):
+    def get(self, request, pk, *args, **kwargs):
+        players = []
+        player_pks = []
+        team_players = Player.objects.filter(team=pk)
+        # get player averages
+        for player in team_players:
+            total = 0
+            pm_cons = MatchPlayerConnection.objects.filter(player=player.pk)
+            # calculate player total
+            for pm_con in pm_cons:
+                total += pm_con.score
+            average = total / player.game_count if player.game_count > 0 else 0
+            players.append({
+                'pk': player.pk,
+                'average': average
+            })
+
+        sorted_players = sorted(players, key=lambda l: l['average'])
+        percentile_90th = math.floor(len(players) * 0.9)
+
+        for i in range(percentile_90th - 1, len(players)):
+            player_pks.append(sorted_players[i]['pk'])
+
+        data = Player.objects.filter(pk__in=player_pks)
+        serializer = PlayerSerializer(data, many=True)
+        return Response({
+            'message': '90th Percentile Players',
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)
